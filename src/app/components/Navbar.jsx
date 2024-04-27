a5645a650c11ebb3a7a565d55cc9883d02c625fc
RESHMA R <reshma24799@gmail.com>
2024-04-27 15:45:16 +0530

    completed navbar for large screen

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import NavLink from "./NavLink"
+
+const Navbar = () => {
+    const navLinks = [
+        {
+          title: "About",
+          path: "#about",
+        },
+        {
+          title: "Projects",
+          path: "#projects",
+        },
+        {
+          title: "Contact",
+          path: "#contact",
+        },
+      ];
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+        <div className="flex flex-wrap container items-center justify-between mx-auto px-4 py-2 lg:py-4">
+            <div></div>
+            <div className="menu hidden md:block md:w-auto " id="navbar">
+                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-10">
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <NavLink href={link.path} title={link.title} />
+                        </li>
+            ))}
+                </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
