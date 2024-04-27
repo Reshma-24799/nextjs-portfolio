7048844cdde5a7fe4b3ff0dda5634456249e0be4
RESHMA R <reshma24799@gmail.com>
2024-04-27 11:11:56 +0530

    setting up project, added header section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Reshma</h1>
+        <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            As a full-stack web developer with three years of experience, I specialize
+            in crafting fast, responsive, and dynamic websites that prioritize user
+            experience above all else. My expertise lies in creating intuitive interfaces
+            that seamlessly engage and empower end-users. Let's collaborate and bring your vision to life!
+        </p>
+    </section>
+  )
+}
+
+export default HeroSection;
